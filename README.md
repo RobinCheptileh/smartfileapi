aa710e6f7c83d391385abad00cc2559eb772b518
Robin Cheptileh <robincheptileh@gmail.com>
2017-08-31 23:52:59 +0300

    Some renames

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ And then execute:
 
 Or install it yourself as:
 
-    $ gem install smartfileapi
+    $ gem install smartfile-api
 
 ## Usage
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require "bundler/setup"
-require "smart_file_api"
+require 'bundler/setup'
+require 'smartfile-api/services'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
