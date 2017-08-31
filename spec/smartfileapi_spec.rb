903c63cb15659b36ad61ba21bfaab5a9b0b03740
Robin Cheptileh <robincheptileh@gmail.com>
2017-08-31 13:19:34 +0300

    Added Figaro in Development to load API keys and pass. Test succeeded

--- spec/smartfileapi_spec.rb
+++ spec/smartfileapi_spec.rb
@@ -1,11 +1,22 @@
-require "spec_helper"
+require 'spec_helper'
+require 'figaro'
 
 RSpec.describe Smartfileapi do
-  it "has a version number" do
-    expect(Smartfileapi::VERSION).not_to be nil
+  before do
+    Figaro.application = Figaro::Application.new(environment: 'development',
+                                                 path: File.expand_path('../../application.yml', __FILE__))
+    Figaro.application.load
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'version' do
+    it 'has a version number' do
+      expect(Smartfileapi::VERSION).not_to be nil
+    end
+  end
+
+  describe 'config' do
+    it 'has been configured properly' do
+      expect(ENV.key?('SMARTFILE_KEY') && ENV.key?('SMARTFILE_PASSWORD')).to be true
+    end
   end
 end
