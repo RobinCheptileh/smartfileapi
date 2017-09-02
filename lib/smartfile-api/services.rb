b30133129aa98f35e4124f5f8b78c52105f1baa4
Robin Cheptileh <robincheptileh@gmail.com>
2017-09-02 15:06:27 +0300

    Edited code, put the bulk in format response

--- lib/smartfile-api/services.rb
+++ lib/smartfile-api/services.rb
@@ -11,37 +11,30 @@ module SmartFileApi
   ACTIVITY_URL = 'activity/'.freeze
   LINK_URL = 'link/'.freeze
   TASK_URL = 'task/'.freeze
+  PATH_INFO_URL = 'path/info/'.freeze
 
   # All services
   class Services
     def initialize
-      raise(ArgumentError, 'SmartFileApi::Services: SmartFile KEY and(or) PASSWORD missing') if !ENV.key?('SMARTFILE_KEY') || !ENV.key?('SMARTFILE_PASSWORD')
+      raise(ArgumentError, 'SmartFileApi::Services.initialize: SmartFile KEY and(or) PASSWORD missing') if !ENV.key?('SMARTFILE_KEY') || !ENV.key?('SMARTFILE_PASSWORD')
       @smartfile_key = ENV['SMARTFILE_KEY'].freeze
       @smartfile_pass = ENV['SMARTFILE_PASSWORD'].freeze
-      raise(ArgumentError, 'SmartFileApi::Services: Authentication failed') if whoami[:code] != 200
+      raise(ArgumentError, 'SmartFileApi::Services.initialize: Authentication failed') if whoami[:code] != 200
     end
 
     # Ping API Server
     def ping
-      response = request_resource(PING_URL).get
-      json = JSON.parse(response.body, symbolize_names: true)
-      put_normals(response).merge(ping: json[:ping])
+      format_response(request_resource(PING_URL).get)
     end
 
     # Get current user's identity
     def whoami
-      response = request_resource(WHOAMI_URL).get
-      json = JSON.parse(response.body, symbolize_names: true)
-      put_normals(response).merge(address: json[:address],
-                                  user: json[:user],
-                                  site: json[:site])
+      format_response(request_resource(WHOAMI_URL).get)
     end
 
     # Get current session expiry in seconds
     def session
-      response = request_resource(SESSION_URL).get
-      json = JSON.parse(response.body, symbolize_names: true)
-      put_normals(response).merge(expires: json[:expires])
+      format_response(request_resource(SESSION_URL).get)
     end
 
     # List specified options
@@ -50,48 +43,34 @@ module SmartFileApi
             when :activities then ACTIVITY_URL
             when :links then LINK_URL
             when :tasks then TASK_URL
-            else raise(ArgumentError, "SmartFileApi::Services.list: Invalid option '#{option}' argument")
+            else raise(ArgumentError, "SmartFileApi::Services.list: Invalid option '#{option}'")
             end
-      response = request_resource(url).get(params: params)
-      json = JSON.parse(response.body, symbolize_names: true)
-      put_normals(response).merge(total: json[:total],
-                                  page: json[:page],
-                                  pages: json[:pages],
-                                  per_page: json[:per_page],
-                                  previous: json[:previous],
-                                  next: json[:next],
-                                  results: json[:results])
+      format_response(request_resource(url).get(params: params))
     end
 
     # Create links
     def create_link(params)
-      response = request_resource(LINK_URL).post(params)
-      json = JSON.parse(response.body, symbolize_names: true)
-      put_normals(response).merge(acl: json[:acl],
-                                  created: json[:created],
-                                  expiration: json[:expiration],
-                                  href: json[:href],
-                                  name: json[:name],
-                                  owner: json[:owner],
-                                  path: json[:path],
-                                  recursive: json[:recursive],
-                                  uid: json[:uid],
-                                  usage_count: json[:usage_count],
-                                  usage_limit: json[:usage_limit],
-                                  cache: json[:cache])
+      format_response(request_resource(LINK_URL).post(params))
+    end
+
+    # Retrieval of a path's metadata
+    def get_path_info(path, params = '')
+      url = PATH_INFO_URL + path
+      format_response(request_resource(url).get(params: params))
     end
 
     private
 
-    def put_normals(response)
-      raise("SmartFileApi::Services: Error code #{response.code}") unless [200, 201].include?(response.code)
-      json = JSON.parse(response.body, symbolize_names: true)
+    # Format the api call response
+    def format_response(response)
+      raise("SmartFileApi::Services.format_response: Error code #{response.code}") unless [200, 201].include?(response.code)
       { code: response.code,
         headers: response.headers,
-        body: response.body,
-        json: json }
+        body: response.body }.merge(JSON.parse(response.body,
+                                               symbolize_names: true))
     end
 
+    # Create a http resource with url and basic auth credentials
     def request_resource(url)
       RestClient::Resource.new(BASE_URL + url, user: @smartfile_key, password: @smartfile_pass)
     end
